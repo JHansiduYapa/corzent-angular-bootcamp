0c5e64302110c2e3cb4b7087a45bb5b0dcdbfe9d
Janith Hansidu Yapa <97206910+JHansiduYapa@users.noreply.github.com>
2025-10-03 10:24:14 +0530

    add reactive forms

--- projects/project-7/src/app/auth/login/login.component.ts
+++ projects/project-7/src/app/auth/login/login.component.ts
@@ -1,23 +1,47 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { of } from 'rxjs';
+
+// custom validator function to email to check it is test.com domain
+function emailIsUnique(control: AbstractControl){
+  if(control.value !== 'test.com'){
+    return of(null);
+  }
+  return of({ emailNotUnique: true });
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
-  onSubmit(formData: NgForm) {
-    
-    if (!formData.valid) {      
-      return;
-    }
-    const enteredEmail = formData.value.email;
-    const enteredPassword = formData.value.password;
-    console.log(enteredEmail, enteredPassword);
-    formData.reset();
+  // Reactive Form for connecting to the template
+  form = new FormGroup({
+    email: new FormControl('',{
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  })
 
+  onSubmit() {
+    // get the form values
+    console.log(this.form.value.email);
+    console.log(this.form.value.password);    
+  } 
+
+  // getter for email field validity
+  get emailIsInvalid() {
+    return this.form.controls.email.invalid && this.form.controls.email.touched;
+  }
+
+  // getter for password field validity
+  get passwordIsInvalid() {
+    return this.form.controls.password.invalid && this.form.controls.password.touched;
   }
 }
